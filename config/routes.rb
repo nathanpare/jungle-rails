360d73b64c6eaf370c4861c96b179b575c99de5a
Nathan Pare <nathanpare@Nathans-MacBook-Pro.local>
2022-07-12 15:08:56 -0700

    Feature: About Page

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-
+  get '/about' => 'about#index'
   root to: 'products#index'
 
   resources :products, only: [:index, :show]
@@ -16,7 +16,6 @@ Rails.application.routes.draw do
     root to: 'dashboard#show'
     resources :products, except: [:edit, :update, :show]
   end
-
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
