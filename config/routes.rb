e39cbd7b0f0fa16e7b1f0277e069f2e21e36b1e1
Nathan Pare <nathanpare@Nathans-MacBook-Pro.local>
2022-07-12 20:36:03 -0700

    Feature: Admin Categories

--- config/routes.rb
+++ config/routes.rb
@@ -15,6 +15,7 @@ Rails.application.routes.draw do
   namespace :admin do
     root to: 'dashboard#show'
     resources :products, except: [:edit, :update, :show]
+    resources :categories, except: [:destroy, :show, :edit, :update]
   end
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
