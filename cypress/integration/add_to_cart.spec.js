6b92da0b8511ec917804040a20ff258bee4bc719
Nathan Pare <nathanpare@Nathans-MacBook-Pro.local>
2022-07-24 16:23:56 -0700

    Feature Spec Add to Cart

---
+++ cypress/integration/add_to_cart.spec.js
@@ -0,0 +1,34 @@
+describe('Jungle Rails', () => {
+  beforeEach(() => {
+    // Cypress starts out with a blank slate for each test
+    // so we must tell it to visit our website with the `cy.visit()` command.
+    // Since we want to visit the same URL at the start of all our tests,
+    // we include it in our beforeEach function so that it runs before each test
+    cy.visit("/")
+  });
+
+  it('displays the home page', () => {
+    // We use the `cy.get()` command to get all elements that match the selector.
+    // Then, we use `should` to assert that there are two matched items,
+    // which are the two default items.
+    cy.get('body')
+
+    // We can go even further and check that the default todos each contain
+    // the correct text. We use the `first` and `last` functions
+    // to get just the first and last matched elements individually,
+    // and then perform an assertion with `should`.
+  });
+
+  it("There is products on the page", () => {
+    cy.get(".products article").should("be.visible");
+  });
+
+  it("There is 2 products on the page", () => {
+    cy.get(".products article").should("have.length", 2);
+  });
+
+  it("Can click the 'Add to Cart' button for a product on the home page, increasing the cart quantity by one", () => {
+    cy.get('.btn').first().click({force:true})   
+    cy.get('.nav-link').contains(1)
+    });
+})
